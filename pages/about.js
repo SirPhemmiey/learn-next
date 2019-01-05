219a71c89dc0dcf7ebf18c16d71fd1f3e242b4cc
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2019-01-05 22:27:32 +0100

    Use next-links in the pages

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
+import Layout from "../comps/MyLayout";
+
 export default () => (
-    <div>
+    <Layout>
         <p>This is the about page</p>
-    </div>
+    </Layout>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 //This is the link API
-import Link from 'next/link';
+import Layout from '../comps/MyLayout';
 
 const Index = () => (
-    <div>
-        <Link href="/about">
-            <a style={{fontSize:20}}>About Page</a>
-        </Link>
-        <p>Hello Next.js</p>
-    </div>
+        <Layout>
+            <p>Hello Next.js</p>
+        </Layout>
 );
 
 export default Index;
