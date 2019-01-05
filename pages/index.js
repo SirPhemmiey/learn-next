4a438492e2b984736d46ed0b980d8e77637330ab
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2019-01-05 17:50:10 +0100

    First commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+//This is the link API
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <Link href="/about">
+            <a style={{fontSize:20}}>About Page</a>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
+);
+
+export default Index;
